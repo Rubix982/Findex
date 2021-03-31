44b94cd9b88fa059c9be3c52c75da2575acd475a
Saif Ul Islam <saifulislam84210@gmail.com>
2021-03-31 11:39:22 +0500

    optimize the generation of positional indexes

--- server/models/preprocess_for_indexes.py
+++ server/models/preprocess_for_indexes.py
@@ -4,6 +4,7 @@ import nltk
 import json
 import re
 import os
+import multiprocessing as mp
 from nltk.stem import WordNetLemmatizer
 from multiprocessing import Pool
 
@@ -89,14 +90,18 @@ file_paths = [
     f"{str(path_name_parent)}/{str(path_name_entry)}" for path_name_entry in path_name_list]
 
 
-def generate_positional_index(str_to_build_positional: str, positional_index: PositionalIndex):
+def process_data_through_pipelines():
 
-    total_positions = ''
+    short_stories_path = f"{dist_path}/ShortStories"
 
-    for path_index, path in enumerate(file_paths):
+    global file_paths
+
+    if not os.path.exists(short_stories_path):
+        os.makedirs(short_stories_path)
+
+    for path in file_paths:
 
         tokens = []
-        positions = ''
 
         with open(path) as file:
 
@@ -108,79 +113,104 @@ def generate_positional_index(str_to_build_positional: str, positional_index: Po
                     data[idx] = data[idx].replace(remove_character, ' ')
                     data[idx] = re.sub(' +', ' ', data[idx].strip())
 
-            data = ' '.join([entry for entry in data if entry != ''])
+            data = ' '.join([entry for entry in data if entry != '' and not entry.isnumeric()])
             tokens = [token.lower() for token in data.split(' ')]
 
-        for idx, token in enumerate(tokens):
-
             # STEMMING ->> LEMMATIZATION
-            token = wordnet_lemmatizer.lemmatize(
-                custom_porter_algorithm(token))
+            tokens = [wordnet_lemmatizer.lemmatize(
+                custom_porter_algorithm(token)) for token in tokens]
 
+            with open(f"{short_stories_path}/{path.split('/')[-1].split('.')[0]}.txt", mode='w') as file:
+                file.write(' '.join(tokens))
+
+
+def generate_positional_index(str_to_build_positional: str, positional_index: PositionalIndex):
+
+    global file_paths
+    short_stories_path = []
+    total_positions = ''
+
+    for path in file_paths:
+        new_path = path.split('/')
+        new_path[-3] = 'dist'
+        short_stories_path.append('/'.join(new_path))
+
+    for file_path_idx, path in enumerate(short_stories_path):
+
+        positions = ''
+    
+        with open(path) as file:
+            tokens = file.readlines()[0].split(' ')
+            
             # TODO - I haven't tested how accurately this works
-            if token == str_to_build_positional:
-                positions += f"{idx},"
+            for idx, token in enumerate(tokens):
+                if str_to_build_positional == token:
+                    positions += f"{idx},"
 
-        if positions == '':
-            continue
-
-        # positional_node = PositionNode(
-        #     doc_id=path.split('/')[-1].split('.')[0], position_list=positions)
-
-        positions = f"{path_index},{positions[0:-1]};"
-        total_positions = total_positions + positions
+        if positions != '':
+            positions = f"{file_path_idx},{positions[0:-1]};"
+            total_positions = total_positions + positions
 
     positional_index.document_listing_positions = total_positions
+
+def for_line_store_file(line: str):
+
+    global unique_word_counter
+
+    # Preprocessing / cleaning
+    entry = line.strip('\n').split(',')[0:2]
+
+    if os.path.exists(f"{positional_indexing_path}/{entry[1]}.txt"):
+        return
+
+    # Determining the csv path
+    # csv_file_path = f"{inverted_indexing_path}/{unique_word_counter+500}-{unique_word_counter+1000}.csv"
+
+    # Creating a new csv file for the words if none was found
+    if int(entry[0]) < unique_word_counter + 500:
+        unique_word_counter += 500
+
+    # generate positional index
+    positional_index = PositionalIndex(
+        unique_hash=f"{entry[0]}")
+
+    # Get the postiings from the parse
+    generate_positional_index(entry[1], positional_index)
+
+    content_dump = positional_index.__dict__
+
+    if content_dump['document_listing_positions'] == '':
+        return
+
+    # opening file to save
+    with open(f"{positional_indexing_path}/{entry[1]}.txt", mode='w') as file:
+        # serializing to json
+        file.write(
+            f"{content_dump['unique_hash']}-{content_dump['document_listing_positions']}")
+
+    # # opening file to save
+    # with open(csv_file_path, 'a') as file:
+    #     file.write(f"{content_dump['unique_keyword']},")
+    #     for i in range(len(file_paths)):
+    #         file.write(
+    #             f"{len(content_dump['document_listing_positions'][i]['position_list'])},")
+    #     file.write('\n')
 
 
 def get_saved_normalized_words():
 
     global unique_word_counter
 
+    total_words_list = []
     with open(f"{dist_path}/Total-Words.csv") as file:
         for line in file:
+            # for_line_store_file(line)
+            total_words_list.append(line)
 
-            # Preprocessing / cleaning
-            entry = line.strip('\n').split(',')[0:2]
-
-            # Determining the csv path
-            csv_file_path = f"{inverted_indexing_path}/{unique_word_counter+500}-{unique_word_counter+1000}.csv"
-
-            # Creating a new csv file for the words if none was found
-            if int(entry[0]) < unique_word_counter + 500:
-                unique_word_counter += 500
-            #     if not os.path.isfile(csv_file_path):
-            #         with open(csv_file_path, 'w') as file:
-            #             file.write(',')
-            #             for i in range(1, 50 + 1):
-            #                 file.write(f"{i},")
-            #             file.write('\n')
-            # else:
-
-            # generate positional index
-            positional_index = PositionalIndex(
-                unique_hash=f"{entry[0]}")
-
-            # Get the postiings from the parse
-            generate_positional_index(entry[1], positional_index)
-
-            content_dump = positional_index.__dict__
-
-            if content_dump['document_listing_positions'] == '':
-                content_dump.pop('document_listing_positions')
-
-            # opening file to save
-            with open(f"{positional_indexing_path}/{entry[1]}.json", mode='w') as file:
-                # serializing to json
-                file.write(json.dumps(content_dump))
-
-            # # opening file to save
-            # with open(csv_file_path, 'a') as file:
-            #     file.write(f"{content_dump['unique_keyword']},")
-            #     for i in range(len(file_paths)):
-            #         file.write(
-            #             f"{len(content_dump['document_listing_positions'][i]['position_list'])},")
-            #     file.write('\n')
+    with mp.Pool(10) as p:
+        p.map(for_line_store_file, total_words_list)
 
 
 get_saved_normalized_words()
+
+# process_data_through_pipelines()
